b9b76ee1f140f99c3ae9d969214cd51afec5c2cc
Yaduvanshi SaiKumar <sai.yaduvanshi@yahoo.com>
2024-06-03 15:21:04 +0530

    TEST

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AnagramPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string:");
+            string input = Console.ReadLine();
+
+            bool result = CanFormPalindrome(input);
+            if (result)
+            {
+                Console.WriteLine("Yes, an anagram of the string can form a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("No, an anagram of the string cannot form a palindrome.");
+            }
+        }
+
+        static bool CanFormPalindrome(string str)
+        {
+            var charFrequency = str.GroupBy(c => c)
+                                   .ToDictionary(g => g.Key, g => g.Count());
+
+            int oddCount = charFrequency.Values.Count(freq => freq % 2 != 0);
+
+            return oddCount <= 1;
+        }
+    }
+}
